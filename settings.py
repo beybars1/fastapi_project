d4b84e7f050ba325353621205a15f7f40c24dc7b
beybars1 <ck002.ck002@gmail.com>
2025-02-18 21:52:07 +0500

    added Dockerfile, updated files

--- settings.py
+++ settings.py
@@ -7,7 +7,13 @@ REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
     default="postgresql+asyncpg://postgres:postgres@127.0.0.1:5434/postgres",
 )  # connect string for the real database
+APP_PORT = env.int("APP_PORT")
 
+
+SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
+ALGORITHM: str = env.str("ALGORITHM", default="HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
+SENTRY_URL: str = env.str("SENTRY_URL")
 
 TEST_DATABASE_URL = env.str(
     "TEST_DATABASE_URL",
